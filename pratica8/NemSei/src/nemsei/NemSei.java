4d568018f63ef7df957e21b3b01eaf09d0462880
Aluno <Aluno>
2017-09-12 10:43:46 -0300

    import de pacote

---
+++ pratica8/NemSei/src/nemsei/NemSei.java
@@ -0,0 +1,9 @@
+package nemsei;
+
+public class NemSei {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ pratica8/Organizando/src/br/com/igao/organizando/Organizando.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.igao.organizando;
+
+import br.com.Rodrigo.organizando.*;
+
+public class Organizando {
+
+    br.com.igao.organizando.Cliente eu = new br.com.igao.organizando.Cliente("potato");
+    
+    public static void main(String[] args) {
+        System.out.println("BATAAAAATAAAA");
+        //System.out.println(eu.getNome());
+    }
+    
+}
